7c5244bc1c2edf0ea302d4b5b1ef6d9ebf42a05c
NiculaGabriel <nicula.gabriel87@yahoo.com>
2021-06-23 11:58:29 +0300

    small change in js file

--- wp-content/plugins/ng_widget_sdebar/public/js/ng_widget_sdebar-public.js
+++ wp-content/plugins/ng_widget_sdebar/public/js/ng_widget_sdebar-public.js
@@ -44,7 +44,7 @@ window.addEventListener('load', function(){
 				return ( document.querySelectorAll('.icon-drop-down') ? document.querySelectorAll('.icon-drop-down') : false );
 			},
 			iterateAllArrowItems:function(list) {
-				if(list && list.length){
+				if(list && list.length) {
 					for( var i in list ){
 						if(list[i].tagName == 'P') {
 							this.addClickEvent(list[i]);
